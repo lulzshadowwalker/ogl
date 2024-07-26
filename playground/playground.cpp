785caf4ef68f92146e4857dc8bd5a55021eb04cb
lulzshadowwalker <lulzshadowwalker@gmail.com>
2024-07-26 11:28:49 +0300

    model view perspective

--- playground/playground.cpp
+++ playground/playground.cpp
@@ -9,13 +9,47 @@
 
 // 3D maths
 #include <glm/glm.hpp>
+#include <glm/gtx/transform.hpp>
 
 #include <common/shader.hpp>
 
 using namespace glm;
 
+// x, y, z, w
+// w: 0  -> direction
+// w: 1  -> position in space
+
+void testPointTranslation()
+{
+    printf("Point Translation:\n");
+    glm::mat4 matrix = glm::translate(glm::mat4(), glm::vec3(10.0f, 0.0f, 0.0f)); // This gives you the translation matrix. it does not actually do the translation
+    glm::vec4 vertex(10.0f, 10.0f, 10.0f, 1.0f); // `w` is 1 then it's a point. you can translate a point.
+    glm::vec4 transformedVertex = matrix * vertex;
+    printf("(%f, %f, %f, %f)\n", transformedVertex.x, transformedVertex.y, transformedVertex.z, transformedVertex.w);
+}
+
+void testDirectionTranslation()
+{
+    printf("Direction Translation:\n");
+    glm::mat4 matrix = glm::translate(glm::mat4(), glm::vec3(10.0f, 0.0f, 0.0f)); // This gives you the translation matrix. it does not actually do the translation
+    // `w` is 0 then it's a direction. you cannot translate a direction. it has no effect.
+    // which makes sense, because `w` being 0 the last item in the vertex, would be multiplied by the translation (last column) in the matrix so you end up simply preserving the original vertex without any translation.
+    glm::vec4 vertex(10.0f, 10.0f, 10.0f, 0.0f);
+    glm::vec4 transformedVertex = matrix * vertex;
+    printf("(%f, %f, %f, %f)\n", transformedVertex.x, transformedVertex.y, transformedVertex.z, transformedVertex.w);
+}
+
+// This line actually performs the scaling FIRST, and THEN the rotation, and THEN the translation. This is how matrix multiplication works.
+// Writing the operations in another order wouldn’t produce the same result.
+//
+// TransformedVector = TranslationMatrix * RotationMatrix * ScaleMatrix * OriginalVector;
+
 int main( void )
 {
+    // testPointTranslation();
+    // testDirectionTranslation();
+    // return 0;
+    
     // Initialise GLFW
     glewExperimental = true; // Needed for core profile
     if( !glfwInit() )
@@ -32,7 +66,7 @@ int main( void )
     
     // Open a window and create its OpenGL context
     GLFWwindow* window; // (In the accompanying source code, this variable is global for simplicity)
-    window = glfwCreateWindow( 1024, 768, "Lulzie's world", NULL, NULL);
+    window = glfwCreateWindow( 1024, 768, "lulzie's world", NULL, NULL);
     if( window == NULL ) 
     {
         fprintf( stderr, "Failed to open GLFW window. If you have an Intel GPU, they are not OpenGL 3.3 compatible. Try the 2.1 version of the tutorials.\n" );
@@ -44,7 +78,7 @@ int main( void )
     glewExperimental = true; // Needed in core profile
     if ( glewInit() != GLEW_OK )
     {
-        fprintf(stderr, "Failed to initialize GLEW\n");
+        fprintf( stderr, "Failed to initialize GLEW\n" );
         return -1;
     }
     
@@ -78,11 +112,39 @@ int main( void )
     
     GLuint programID = LoadShaders( "/Users/lulz/dev/ogl/SimpleVertexShader.vertexshader", "/Users/lulz/dev/ogl/SimpleFragmentShader.fragmentshader" );
     
+    // Projection matrix: 45° Field of View, 4:3 ratio, display range: 0.1 unit <-> 100 units
+    float width = 200;
+    float height = 200;
+    glm::mat4 Projection = glm::perspective(glm::radians(45.0f), width / height, 0.1f, 100.0f);
+
+    // Or, for an ortho camera:
+    //glm::mat4 Projection = glm::ortho(-10.0f,10.0f,-10.0f,10.0f,0.0f,100.0f); // In world coordinates
+
+    // Camera matrix
+    glm::mat4 View = glm::lookAt(
+        glm::vec3(4,3,3), // Camera is at (4,3,3), in World Space
+        glm::vec3(0,0,0), // and looks at the origin
+        glm::vec3(0,1,0)  // Head is up (set to 0,-1,0 to look upside-down)
+        );
+
+    // Model matrix: an identity matrix (model will be at the origin)
+    glm::mat4 Model = glm::mat4(1.0f);
+    // Our ModelViewProjection: multiplication of our 3 matrices
+    glm::mat4 mvp = Projection * View * Model; // Remember, matrix multiplication is the other way around
+    
+    // Get a handle for our "MVP" uniform
+    // Only during the initialisation
+    GLuint MatrixID = glGetUniformLocation(programID, "MVP");
+    
     // Ensure we can capture the escape key being pressed below
     glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
     do{
         glClear( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT );
         glUseProgram( programID );
+        
+        // Send our transformation to the currently bound shader, in the "MVP" uniform
+        // This is done in the main loop since each model will have a different MVP matrix (At least for the M part)
+        glUniformMatrix4fv(MatrixID, 1, GL_FALSE, &mvp[0][0]);
         
         { // Draw
             // 1st attribute buffer : vertices
@@ -100,7 +162,6 @@ int main( void )
             glDrawArrays(GL_TRIANGLES, 0, 3); // Starting from vertex 0; 3 vertices total -> 1 triangle
             glDisableVertexAttribArray(0);
         }
-
         
         // Swap buffers
         glfwSwapBuffers(window);
